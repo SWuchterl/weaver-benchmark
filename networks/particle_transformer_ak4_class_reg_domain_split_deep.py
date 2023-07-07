8f7b592facce841d94645314b57e08bf390343b2
Raffaele <raffaele.gerosa@cern.ch>
2023-07-07 15:57:05 +0200

    adding a more deep model

--- networks/particle_transformer_ak4_class_reg_domain_split_deep.py
+++ networks/particle_transformer_ak4_class_reg_domain_split_deep.py
@@ -32,16 +32,15 @@ def get_model(data_config, **kwargs):
         num_classes = num_classes,
         num_targets = num_targets,
         num_domains = num_domains,
-        save_grad_inputs = True,
         pair_input_dim = len(data_config.input_dicts['pf_vectors']),
         pair_extra_dim = 0,
         embed_dims = [128, 256, 128],
         pair_embed_dims = [64, 64, 64],
         block_params = None,
         cls_block_params={'dropout': 0, 'attn_dropout': 0, 'activation_dropout': 0},
-        num_heads = kwargs.get('num_heads',8),
-        num_layers = kwargs.get('num_layers',8),
-        num_cls_layers = kwargs.get('num_cls_layers',2),
+        num_heads = kwargs.get('num_heads',10),
+        num_layers = kwargs.get('num_layers',10),
+        num_cls_layers = kwargs.get('num_cls_layers',4),
         remove_self_pair = kwargs.get('remove_self_pair',False),
         use_pre_activation_pair = kwargs.get('use_pre_activation_pair',True),
         activation = kwargs.get('activation','gelu'),
@@ -51,7 +50,7 @@ def get_model(data_config, **kwargs):
         use_amp = kwargs.get('use_amp',False),
         split_domain_outputs = kwargs.get('split_domain_outputs',False),
         fc_params = [(256, 0.1), (128, 0.1), (96, 0.1), (64, 0.1)],
-        fc_domain_params = [(256, 0.1), (128, 0.1), (96, 0.1), (64, 0.1)]
+        fc_domain_params = [(128, 0.1), (96, 0.1), (64, 0.1)]
     );
 
     model = ParticleTransformerTagger(**cfg)
@@ -66,46 +65,41 @@ def get_model(data_config, **kwargs):
     return model, model_info
 
 
-class CrossEntropyLogCoshLossDomainFgsm(torch.nn.L1Loss):
-    __constants__ = ['reduction','loss_lambda','loss_gamma','quantiles','loss_kappa','domain_weight','domain_dim','loss_omega']
+class CrossEntropyLogCoshLossDomain(torch.nn.L1Loss):
+    __constants__ = ['reduction','loss_lambda','loss_gamma','quantiles','loss_kappa','domain_weight','domain_dim']
 
     def __init__(self, 
                  reduction: str = 'mean', 
                  loss_lambda: float = 1., 
                  loss_gamma: float = 1., 
                  loss_kappa: float = 1., 
-                 loss_omega: float = 1.,
-                 fgsm_loss: str = '',
                  quantiles: list = [],
                  domain_weight: list = [],
-                 domain_dim: list = []
+                 domain_dim: list = [],
              ) -> None:
-        super(CrossEntropyLogCoshLossDomainFgsm, self).__init__(None, None, reduction)
+        super(CrossEntropyLogCoshLossDomain, self).__init__(None, None, reduction)
         self.loss_lambda = loss_lambda;
         self.loss_gamma = loss_gamma;
         self.loss_kappa = loss_kappa;
-        self.loss_omega = loss_omega;
         self.quantiles = quantiles;
         self.domain_weight = domain_weight;
         self.domain_dim = domain_dim;
-        self.fgsm_loss = fgsm_loss;
-        
+
     def forward(self, 
                 input_cat: Tensor, y_cat: Tensor, 
                 input_reg: Tensor, y_reg: Tensor, 
-                input_domain: Tensor, y_domain: Tensor, y_domain_check: Tensor,
-                input_cat_fgsm: Tensor, input_cat_ref: Tensor) -> Tensor:
+                input_domain: Tensor, y_domain: Tensor, y_domain_check: Tensor) -> Tensor:
 
         ## classification term
-        loss_cat = 0;
+        loss_cat  = 0;
         if input_cat.nelement():
             loss_cat = torch.nn.functional.cross_entropy(input_cat,y_cat,reduction=self.reduction);
 
         ## regression terms
-        x_reg = input_reg-y_reg;        
-        loss_mean = 0;
+        x_reg      = input_reg-y_reg;        
+        loss_mean  = 0;
         loss_quant = 0;
-        loss_reg = 0;
+        loss_reg   = 0;
         if input_reg.nelement():
             ## compute loss
             for idx,q in enumerate(self.quantiles):
@@ -130,7 +124,7 @@ class CrossEntropyLogCoshLossDomainFgsm(torch.nn.L1Loss):
             loss_reg = self.loss_lambda*loss_mean+self.loss_gamma*loss_quant;
 
         ## domain terms
-        loss_domain = 0;
+        loss_domain    = 0;
         if input_domain.nelement():
             ## just one domain region
             if not self.domain_weight or len(self.domain_weight) == 1:
@@ -146,22 +140,9 @@ class CrossEntropyLogCoshLossDomainFgsm(torch.nn.L1Loss):
                     if y_val.nelement():
                         loss_domain += w*torch.nn.functional.cross_entropy(y_val,y_pred,reduction=self.reduction);
                 loss_domain *= self.loss_kappa;
+            
+        return loss_cat+loss_reg+loss_domain, loss_cat, loss_reg, loss_domain;
 
-        ## fgsm term
-        loss_fgsm = 0;
-        if input_cat_fgsm.nelement() and input_cat_ref.nelement():
-            if self.fgsm_loss == 'MSE':
-                loss_fgsm = self.loss_omega*torch.nn.functional.mse_loss(                    
-                    input=torch.softmax(input_cat_fgsm,dim=1),
-                    target=torch.softmax(input_cat_ref,dim=1),
-                    reduction='sum');
-            elif self.fgsm_loss == 'KL' or self.fgsm_loss == '':
-                loss_fgsm = self.loss_omega*torch.nn.functional.kl_div(
-                    input=torch.softmax(input_cat_fgsm,dim=1),
-                    target=torch.softmax(input_cat_ref,dim=1),
-                    log_target=True,reduction='sum').abs();
-        return loss_cat+loss_reg+loss_domain+loss_fgsm, loss_cat, loss_reg, loss_domain, loss_fgsm;
-    
 def get_loss(data_config, **kwargs):
 
     ## number of targets
@@ -174,13 +155,11 @@ def get_loss(data_config, **kwargs):
     else:
         ldomain = [len(data_config.label_domain_value)];
 
-    return CrossEntropyLogCoshLossDomainFgsm(
+    return CrossEntropyLogCoshLossDomain(
         reduction=kwargs.get('reduction','mean'),
         loss_lambda=kwargs.get('loss_lambda',1),
         loss_gamma=kwargs.get('loss_gamma',1),
         loss_kappa=kwargs.get('loss_kappa',1),
-        loss_omega=kwargs.get('loss_omega',1),
-        fgsm_loss='',
         quantiles=quantiles,
         domain_weight=wdomain,
         domain_dim=ldomain
