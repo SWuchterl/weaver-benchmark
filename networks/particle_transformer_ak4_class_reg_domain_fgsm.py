cf5ea33a653467eaba94fbb4b5881d604d58b3bb
Raffaele <raffaele.gerosa@cern.ch>
2023-09-28 15:38:46 +0200

    modifying split regression nodes

--- networks/particle_transformer_ak4_class_reg_domain_fgsm.py
+++ networks/particle_transformer_ak4_class_reg_domain_fgsm.py
@@ -50,6 +50,7 @@ def get_model(data_config, **kwargs):
         alpha_grad = kwargs.get('alpha_grad',1),
         use_amp = kwargs.get('use_amp',False),
         split_domain_outputs = kwargs.get('split_domain_outputs',False),
+        split_reg_outputs = kwargs.get('split_reg_outputs',False),
         fc_params = [(256, 0.1), (128, 0.1), (96, 0.1), (64, 0.1)],
         fc_domain_params = [(128, 0.1), (96, 0.1), (64, 0.1)]
     );
