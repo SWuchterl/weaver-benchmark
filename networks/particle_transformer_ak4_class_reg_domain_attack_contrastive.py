ee52fcdca27a1465d58c7a5960b1a549c0cff86f
Raffaele <raffaele.gerosa@cern.ch>
2023-11-09 09:47:55 +0100

    bug fix

--- networks/particle_transformer_ak4_class_reg_domain_attack_contrastive.py
+++ networks/particle_transformer_ak4_class_reg_domain_attack_contrastive.py
@@ -2,7 +2,6 @@ import numpy as np
 import math
 import torch
 from torch import Tensor
-from torch import nn
 from nn.model.ParticleTransformerV2 import ParticleTransformerTagger
 
 def get_model(data_config, **kwargs):
@@ -69,17 +68,16 @@ def get_model(data_config, **kwargs):
     return model, model_info
 
 ## https://github.com/yiftachbeer/mmd_loss_pytorch
-class RBF(nn.Module):
+class RBF(torch.nn.Module):
     def __init__(self, n_kernels=5, mul_factor=2.0, bandwidth=None):
         super().__init__()
         self.bandwidth_multipliers = mul_factor ** (torch.arange(n_kernels) - n_kernels // 2)
         self.bandwidth = bandwidth
-
+        
     def get_bandwidth(self, L2_distances):
         if self.bandwidth is None:
             n_samples = L2_distances.shape[0]
-            return L2_distances.data.sum() / (n_samples ** 2 - n_samples)
-
+            return L2_distances.data.sum() / (n_samples ** 2 - n_samples)    
         return self.bandwidth
 
     def forward(self, X):
@@ -87,8 +85,9 @@ class RBF(nn.Module):
         L2_distances = L2_distances.to(X.device,non_blocking=True);
         self.bandwidth_multipliers = self.bandwidth_multipliers.to(X.device,non_blocking=True);
         return torch.exp(-L2_distances[None, ...] / (self.get_bandwidth(L2_distances) * self.bandwidth_multipliers)[:, None, None]).sum(dim=0)
+
 ## MMDLoss
-class MMDLoss(nn.Module):
+class MMDLoss(torch.nn.Module):
 
     def __init__(self, kernel=RBF()):
         super().__init__()
