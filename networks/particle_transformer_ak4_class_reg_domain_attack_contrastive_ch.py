e534269e02bfe892f7d2ba10d3fa0af85f69396a
Raffaele <raffaele.gerosa@cern.ch>
2023-11-11 15:01:20 +0100

    updates

--- networks/particle_transformer_ak4_class_reg_domain_attack_contrastive_ch.py
+++ networks/particle_transformer_ak4_class_reg_domain_attack_contrastive_ch.py
@@ -36,7 +36,7 @@ def get_model(data_config, **kwargs):
         save_grad_inputs = False,
         pair_input_dim = len(data_config.input_dicts['pf_ch_vectors']),
         pair_extra_dim = 0,
-        embed_dims = [64, 128, 64],
+        embed_dims = [128, 256, 128],
         pair_embed_dims = [64, 64, 64],
         block_params = None,
         cls_block_params={'dropout': 0.05, 'attn_dropout': 0.05, 'activation_dropout': 0.05},
